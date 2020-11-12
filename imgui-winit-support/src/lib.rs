498f1e67720d8af70b2cf82448e6bd80c3a850a6
Marijn Suijten <marijn@traverseresearch.nl>
2020-11-12 23:39:15 +0100

    HACK: Run doctest on all winits except 19

--- imgui-winit-support/src/lib.rs
+++ imgui-winit-support/src/lib.rs
@@ -12,66 +12,80 @@
 //! 4. Call frame preparation callback (every frame)
 //! 5. Call render preparation callback (every frame)
 //!
-//! ## Complete example for winit 0.20+ (without a renderer)
-//!
-//! ```no_run
-//! # use winit_23 as winit;
-//! use imgui::Context;
-//! use imgui_winit_support::{HiDpiMode, WinitPlatform};
-//! use std::time::Instant;
-//! use winit::event::{Event, WindowEvent};
-//! use winit::event_loop::{ControlFlow, EventLoop};
-//! use winit::window::{Window};
-//!
-//! let mut event_loop = EventLoop::new();
-//! let mut window = Window::new(&event_loop).unwrap();
-//!
-//! let mut imgui = Context::create();
-//! // configure imgui-rs Context if necessary
-//!
-//! let mut platform = WinitPlatform::init(&mut imgui); // step 1
-//! platform.attach_window(imgui.io_mut(), &window, HiDpiMode::Default); // step 2
-//!
-//! let mut last_frame = Instant::now();
-//! let mut run = true;
-//! event_loop.run(move |event, _, control_flow| {
-//!     match event {
-//!         Event::NewEvents(_) => {
-//!             // other application-specific logic
-//!             let now = Instant::now();
-//!             imgui.io_mut().update_delta_time(now - last_frame);
-//!             last_frame = now;
-//!         },
-//!         Event::MainEventsCleared => {
-//!             // other application-specific logic
-//!             platform.prepare_frame(imgui.io_mut(), &window) // step 4
-//!                 .expect("Failed to prepare frame");
-//!             window.request_redraw();
-//!         }
-//!         Event::RedrawRequested(_) => {
-//!             let ui = imgui.frame();
-//!             // application-specific rendering *under the UI*
-//!
-//!             // construct the UI
-//!
-//!             platform.prepare_render(&ui, &window); // step 5
-//!             // render the UI with a renderer
-//!             let draw_data = ui.render();
-//!             // renderer.render(..., draw_data).expect("UI rendering failed");
-//!
-//!             // application-specific rendering *over the UI*
-//!         },
-//!         Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
-//!             *control_flow = ControlFlow::Exit;
-//!         }
-//!         // other application-specific event handling
-//!         event => {
-//!             platform.handle_event(imgui.io_mut(), &window, &event); // step 3
-//!             // other application-specific event handling
-//!         }
-//!     }
-//! })
-//! ```
+#![cfg_attr(
+    not(feature = "winit-19"),
+    doc = r#"
+## Complete example for winit 0.20+ (without a renderer)
+
+```no_run
+# // TODO: Can't `use imgui_winit_support as winit;` because it must be `pub`. Are we fine with re-exporting?
+# #[cfg(feature = "winit-20")]
+# use winit_20 as winit;
+#
+# #[cfg(feature = "winit-22")]
+# use winit_22 as winit;
+#
+# #[cfg(feature = "winit-23")]
+# use winit_23 as winit;
+
+use imgui::Context;
+use imgui_winit_support::{HiDpiMode, WinitPlatform};
+use std::time::Instant;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window};
+
+let mut event_loop = EventLoop::new();
+let mut window = Window::new(&event_loop).unwrap();
+
+let mut imgui = Context::create();
+// configure imgui-rs Context if necessary
+
+let mut platform = WinitPlatform::init(&mut imgui); // step 1
+platform.attach_window(imgui.io_mut(), &window, HiDpiMode::Default); // step 2
+
+let mut last_frame = Instant::now();
+let mut run = true;
+event_loop.run(move |event, _, control_flow| {
+    match event {
+        Event::NewEvents(_) => {
+            // other application-specific logic
+            let now = Instant::now();
+            imgui.io_mut().update_delta_time(now - last_frame);
+            last_frame = now;
+        },
+        Event::MainEventsCleared => {
+            // other application-specific logic
+            platform.prepare_frame(imgui.io_mut(), &window) // step 4
+                .expect("Failed to prepare frame");
+            window.request_redraw();
+        }
+        Event::RedrawRequested(_) => {
+            let ui = imgui.frame();
+            // application-specific rendering *under the UI*
+
+            // construct the UI
+
+            platform.prepare_render(&ui, &window); // step 5
+            // render the UI with a renderer
+            let draw_data = ui.render();
+            // renderer.render(..., draw_data).expect("UI rendering failed");
+
+            // application-specific rendering *over the UI*
+        },
+        Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        // other application-specific event handling
+        event => {
+            platform.handle_event(imgui.io_mut(), &window, &event); // step 3
+            // other application-specific event handling
+        }
+    }
+})
+```
+"#
+)]
 
 #[cfg(feature = "winit-19")]
 use winit_19 as winit;
